a25be86c51c7f44f2ee15fa195452fa766f6470d
hypeagle <554616332@qq.com>
2017-12-29 15:00:01 +0800

    Modify Android native lib's name.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ JNICALL
 Java_com_hypeagle_androidlearning_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "手持两把锟斤拷，口中疾呼烫烫烫；\n脚踏千朵屯屯屯，笑看万物锘锘锘。";
     return env->NewStringUTF(hello.c_str());
 }
